61dd26e4428e55e93fb3f0452e8640615df286a8
VitorBomfim-12 <vbomfimcunha@gmail.com>
2025-10-23 22:43:46 -0300

    fix: correção da função de soma

--- src/projeto/function.py
+++ src/projeto/function.py
@@ -1,5 +1,9 @@
 def calc_media(lista_de_notas):
+   total_notas=0
    n_elementos = len(lista_de_notas)
-   total_notas = sum(float(lista_de_notas))
+   
+   for i in lista_de_notas:
+      total_notas+=float(i)
+
    media = total_notas/n_elementos
    return media
