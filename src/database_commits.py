785658847cd70266f3f28219aa5b01654293ab6b
VitorBomfim-12 <vbomfimcunha@gmail.com>
2025-10-28 10:25:46 -0300

    teste: inserção dos projetos de teste no banco de dados.

--- src/database_commits.py
+++ src/database_commits.py
@@ -1,23 +1,15 @@
 import os, dotenv
-import secrets
 from projeto import database,app
-from projeto.models import Adm_User, FormsNotas,Projetos
+from projeto.models import Adm_User,Projetos
 from projeto import bcrypt
 
-dotenv.load_dotenv()
-senha= os.getenv('SENHA_ADM')
 
-senha_hash = bcrypt.generate_password_hash(senha).decode('utf-8')
-print(senha_hash)
 with app.app_context():
- credenciais_adm = Adm_User(user_db='cliente-adm',
- password_db=senha_hash)
- 
+
  projetoteste=Projetos(
- nome_projeto = "Projeto padrão de teste"
+ nome_projeto = "Projeto Daily Sync"
 )
  database.session.add(projetoteste) 
- database.session.add(credenciais_adm)
  database.session.commit()
 
 #deixar session commit na mesma identação
