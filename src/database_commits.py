35b47254a6a3cf35bd7977e54fad10bbb980aa3c
Tais Souza <tata.ssouz47@gmail.com>
2025-11-06 09:42:27 -0300

    test: Teste para inserção do banco de dados na AWS

--- src/database_commits.py
+++ src/database_commits.py
@@ -2,14 +2,20 @@ import os, dotenv
 from projeto import database,app
 from projeto.models import Adm_User,Projetos
 from projeto import bcrypt
+from dotenv import load_dotenv
 
+
+dotenv.load_dotenv()
+
+senha = os.getenv('SENHA_ADM')
+
+senha_cripto =bcrypt.generate_password_hash(senha)
 
 with app.app_context():
-
- projetoteste=Projetos(
- nome_projeto = "Projeto Daily Sync"
+ user=Adm_User(
+ user_db="cliente-adm",
+ password_db=senha_cripto
+ 
 )
- database.session.add(projetoteste) 
- database.session.commit()
 
 #deixar session commit na mesma identação
